49d26eef7fa48ee4fd3e6b41a6036e590e3d45e9
c5sire <rsimon64@gmail.com>
2018-10-18 19:32:31 -0500

    ncbi demo

---
+++ dia04/ncbi_demo.R
@@ -0,0 +1,31 @@
+install.packages("rentrez")
+install.packages("magrittr")
+install.packages("seqinr")
+
+library(rentrez)
+library(magrittr)
+library(seqinr)
+
+# mostrar bases de datos
+entrez_dbs()
+
+# como puedo buscar una base de datos
+entrez_db_searchable(db = "nucleotide")
+
+r_search <- entrez_search(db = "nucleotide", term = "Solanum[ORGN] & CYTB[GEN]")
+r_search
+
+r_search$id
+
+entrez_fetch("nucleotide", r_search$ids[1], rettype = "gb") %>% 
+  write(file = "dia04/cytb_1.gb")
+
+entrez_fetch("nucleotide", r_search$ids, rettype = "fasta") %>% 
+  write(file = "dia04/cytb.fa")
+
+fa <- seqinr::read.fasta("dia04/cytb.fa")
+
+fl <- lapply(fa, length) %>% as.integer()
+fn <- names(fa)
+
+dotchart(x = fl, labels = fn)

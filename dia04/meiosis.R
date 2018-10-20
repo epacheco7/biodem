314d15f6ca776a83a9b708671e60944066b58c48
rsimon64 <rsimon64@gmail.com>
2018-10-19 20:17:53 -0500

    mejorando la funcion meiosis

--- dia04/meiosis.R
+++ dia04/meiosis.R
@@ -1,6 +1,7 @@
 meiosis <- function(GM, GP) {
   de_GM <- rbinom(length(GM), 1, 0.5)
   GH <- GP
-  GH[de_GM == 1] <- GM[de_GM == 1]
+  crossover <- de_GM == 1
+  GH[crossover] <- GM[crossover]
   return(GH)
 }
--- dia04/meiosis_demo.R
+++ dia04/meiosis_demo.R
@@ -3,16 +3,18 @@ n <- 10
 madre <- rep("a", n)
 padre <- rep("A", n)
 
+set.seed(123)
 de_madre <- rbinom(n, 1, 0.5)
-
 hijos <- padre
-hijos[de_madre == 1] <- madre[de_madre == 1]
+crossover <- de_madre == 1
+hijos[crossover] <- madre[crossover]
 hijos
 
 meiosis <- function(GM, GP) {
   de_GM <- rbinom(length(GM), 1, 0.5)
   GH <- GP
-  GH[de_GM == 1] <- GM[de_GM == 1]
+  crossover <- de_GM == 1
+  GH[crossover] <- GM[crossover]
   return(GH)
 }
 
